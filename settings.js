22a26d311d83cee32acf1a002b983810ff037e02
Chris Lauer <Chris.Lauer@noaa.gov>
2020-08-13 10:11:18 -0600

    Changing to local by default instead of central

--- settings.js
+++ settings.js
@@ -1,5 +1,6 @@
 
-var stdOffset = 6; 
+// Comment this line in and set it to the offset in standard time (EST: 5, CST: 6, MST: 7, PST: 8)
+// var stdOffset = 6; 
 var timezoneImageLST = "digital-clock2/ccst.gif";
 var timezoneImageLDT = "digital-clock2/ccdt.gif";
 

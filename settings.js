255dd486678947033e9dfdc7882b15f858dd5e56
John Sullivan <john.sullivan@noaa.gov>
2020-08-13 17:10:56 -0500

    Update settings to be more error friendly.

--- settings.js
+++ settings.js
@@ -3,16 +3,17 @@
 /*** utc-lcl.html clock settings ***/
 // The stdOffset variable is only used to customize the local timezone image in the lower left of the utc-lcl.html clock.
 // Comment this line in and set it to the offset in standard time (EST: 5, CST: 6, MST: 7, PST: 8)
-//var stdOffset = 6; 
-// When stdOffset is set, the images for the local timezones must be set below.  
+// When stdOffset is set, the images for the local timezones must be set as well.  
 // If stdOffset is not set, these images are not used, and "LOCAL" will be displayed for timezone. */
 // central
 /*
+var stdOffset = 6; 
 var timezoneImageLST = "digital-clock2/ccst.gif";
 var timezoneImageLDT = "digital-clock2/ccdt.gif";
 */
 // eastern
 /*
+var stdOffset = 5; 
 var timezoneImageLST = "digital-clock2/cest.gif";
 var timezoneImageLDT = "digital-clock2/cedt.gif";
 */
